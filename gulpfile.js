47b4185ba749949f275abe0ecbfc85a6a5f64768
Iris <mdza.iris@gmail.com>
2017-01-25 14:52:30 -0800

    Installed Sass

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var gulp = require('gulp'),
     rename = require('gulp-rename'),
     browserSync = require('browser-sync'),
     eslint = require('gulp-eslint'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
     plumber = require('gulp-plumber'),
     notify = require('gulp-notify');
 
@@ -15,7 +18,6 @@ var plumberErrorHandler = {
 
 gulp.task('scripts', function() {
     gulp.src('./js/*.js')
-        .pipe(plumber(plumberErrorHandler))
         .pipe(uglify())
         .pipe(rename({ extname: '.min.js' }))
         .pipe(gulp.dest('./build/js'))
@@ -32,13 +34,28 @@ gulp.task('browser-sync', function() {
 
 gulp.task('watch', function() {
    gulp.watch('js/*.js', ['scripts']);
+   gulp.watch('sass/*.scss', ['sass']);
 });
 
 gulp.task('lint', function() {
     return gulp.src(['./js/*.js'])
+        .pipe(plumber(plumberErrorHandler))
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
+});
+
+gulp.task('sass', function() {
+   gulp.src('./sass/style.scss')
+      .pipe(plumber(plumberErrorHandler))
+      .pipe(sass())
+      .pipe(autoprefixer({
+         browsers: ['last 2 versions']
+      }))
+      .pipe(gulp.dest('./build/css'))
+      .pipe(cssnano())
+      .pipe(rename('style.min.css'))
+      .pipe(gulp.dest('./build/css'));
 });
 
 gulp.task('default', ['watch', 'browser-sync', 'lint']);
--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 $(function() {
     // this is my message
-    var message = 'Hello world';
-    console.log(message);
-    why is this
+    // var message = 'Hello world';
+    // console.log(message);
+    $('.dropdown-menu').on('select', function() {
+        $.ajax({
+            method: 'GET',
+            url: 'https://'
+        })
+        .done(function(result) {
+            console.log('hello');
+        })
+        .fail(function(err) {
+        })
+    })
 });
