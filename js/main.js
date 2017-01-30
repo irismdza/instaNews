77388969729316ffc7713483e19e67d649452de2
Iris <mdza.iris@gmail.com>
2017-01-30 15:55:54 -0800

    Append article list

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
 $(function() {
-
-    var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
-    url += '?' + $.param({
-  'api-key': "bc19b3c20a46467681e96804ed0ee1dd"
-        });
     $('.dropdown-menu').on('change', function() {
+        var $section = $('.dropdown-menu').val();
+        var url = 'https://api.nytimes.com/svc/topstories/v2/' + $section + '.json';
+        url += '?' + $.param({ 'api-key': 'bc19b3c20a46467681e96804ed0ee1dd'
+        });
         $.ajax({
             method: 'GET',
             url: url
         })
-        .done(function(arrayData) {
-            var onlyResults = arrayData.results.filter(function(result) {
+        .done(function(articleData) {
+            var articleList = ''
+            var filteredData = articleData.results.filter(function(result) {
                 return result.multimedia.length >= 5; 
-            }).slice( 0, 12 );
-    console.log(onlyResults);
-        });
+            }).slice(0, 12);
+            $.each(filteredData, function(key, value) {
+                articleList += '<li>';
+                articleList += '<a href="' + value.url + '">';
+                articleList += '<img src="' + value.multimedia[4].url + '" />';
+                articleList += '<p>' + value.abstract + '</p>';
+                articleList += '</a></li>';
+            });
+         $('.article-list').append(articleList);
     });
+});
 });
