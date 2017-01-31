76cdbbf29488ec42a3ea421c17fa3001c7e46317
Iris <mdza.iris@gmail.com>
2017-01-30 18:04:07 -0800

    Empty article list on option change

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 $(function() {
     $('.dropdown-menu').on('change', function() {
+        var $articleList = $('.article-list');
+        $articleList.empty();
+
         var $section = $('.dropdown-menu').val();
         var url = 'https://api.nytimes.com/svc/topstories/v2/' + $section + '.json';
         url += '?' + $.param({ 'api-key': 'bc19b3c20a46467681e96804ed0ee1dd'
@@ -17,8 +20,8 @@ $(function() {
                 var imageUrl = value.multimedia[4].url;
                 articleList += '<li>';
                 articleList += '<a href="' + value.url + '">';
-                articleList += '<div class="container" style="background-image:url(' + imageUrl + ')">'
-                articleList += '<p class="gradient">' + value.abstract + '</p>';
+                articleList += '<div class="article-grid" style="background-image:url(' + imageUrl + ')">'
+                articleList += '<p class="article-abstract">' + value.abstract + '</p>';
                 articleList += '</div></a></li>';
             });
         $('.article-list').append(articleList);
