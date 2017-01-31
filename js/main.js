1a2cb5c010bef5f97a8cac901466806640cf2b19
Iris <mdza.iris@gmail.com>
2017-01-30 20:59:49 -0800

    Added loader gif on change event

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-$(function() {
-    $('.dropdown-menu').on('change', function() {
+$(function () {
+    $('.dropdown-menu').on('change', function () {
         var $articleList = $('.article-list');
         $articleList.empty();
 
@@ -7,26 +7,38 @@ $(function() {
 
         var $section = $('.dropdown-menu').val();
         var url = 'https://api.nytimes.com/svc/topstories/v2/' + $section + '.json';
-        url += '?' + $.param({ 'api-key': 'bc19b3c20a46467681e96804ed0ee1dd'
+        url += '?' + $.param({
+            'api-key': 'bc19b3c20a46467681e96804ed0ee1dd'
         });
+
+        $('.loader').toggle();
+
+
         $.ajax({
             method: 'GET',
             url: url
         })
-        .done(function(articleData) {
-            var articleList = ''
-            var filteredData = articleData.results.filter(function(result) {
-                return result.multimedia.length >= 5; 
-            }).slice(0, 12);
-            $.each(filteredData, function(key, value) {
-                var imageUrl = value.multimedia[4].url;
-                articleList += '<li>';
-                articleList += '<a href="' + value.url + '">';
-                articleList += '<div class="article-grid" style="background-image:url(' + imageUrl + ')">'
-                articleList += '<p class="article-abstract">' + value.abstract + '</p>';
-                articleList += '</div></a></li>';
-            });
-        $('.article-list').append(articleList);
-        });
+            .done(function (articleData) {
+                var articleList = ''
+                var filteredData = articleData.results.filter(function (result) {
+                    return result.multimedia.length >= 5;
+                }).slice(0, 12);
+                $.each(filteredData, function (key, value) {
+                    var imageUrl = value.multimedia[4].url;
+                    articleList += '<li>';
+                    articleList += '<a href="' + value.url + '">';
+                    articleList += '<div class="article-grid" style="background-image:url(' + imageUrl + ')">'
+                    articleList += '<p class="article-abstract">' + value.abstract + '</p>';
+                    articleList += '</div></a></li>';
+                });
+                $('.article-list').append(articleList);
+
+            })
+        .fail(function () {
+            $('.article-list').append('Sorry there was an error.');
+        })
+        .always(function () {
+            $('.loader').toggle();
+        })
     });
 });
