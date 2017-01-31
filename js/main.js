6cf49e33c1eddc7446364e165c04827db7c682e3
Iris <mdza.iris@gmail.com>
2017-01-30 21:22:53 -0800

    Added selectric plugin

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 $(function () {
+    //call selectric plugin for dropdown menu
+    $('select').selectric();
+
     $('.dropdown-menu').on('change', function () {
         var $articleList = $('.article-list');
-        $articleList.empty();
+        $articleList.empty(); //empty article list
 
+        //shrink header size
         $('.large-header').addClass('small-header');
 
         var $section = $('.dropdown-menu').val();
@@ -11,8 +15,8 @@ $(function () {
             'api-key': 'bc19b3c20a46467681e96804ed0ee1dd'
         });
 
+        //show loader gif
         $('.loader').toggle();
-
 
         $.ajax({
             method: 'GET',
@@ -23,20 +27,27 @@ $(function () {
                 var filteredData = articleData.results.filter(function (result) {
                     return result.multimedia.length >= 5;
                 }).slice(0, 12);
+
                 $.each(filteredData, function (key, value) {
                     var imageUrl = value.multimedia[4].url;
+
                     articleList += '<li>';
                     articleList += '<a href="' + value.url + '">';
                     articleList += '<div class="article-grid" style="background-image:url(' + imageUrl + ')">'
                     articleList += '<p class="article-abstract">' + value.abstract + '</p>';
                     articleList += '</div></a></li>';
                 });
+
                 $('.article-list').append(articleList);
 
             })
+
+        //error message on fail
         .fail(function () {
             $('.article-list').append('Sorry there was an error.');
         })
+
+        //hide loader gif 
         .always(function () {
             $('.loader').toggle();
         })
